8fa473104c1583a025f1e54071ad01d6289b80fd
astr1x <lucaslupus1@gmail.com>
2017-06-29 22:24:57 -0300

    Comitando classe de ajudar a persistencia SQLite

---
+++ app/src/main/java/br/ufg/inf/infosaude/model/User.java
@@ -0,0 +1,55 @@
+package br.ufg.inf.infosaude.model;
+
+/**
+ * Created by astr1x on 29/06/17.
+ */
+
+public class User {
+
+    private Long id;
+    private String email;
+    private String password;
+    private String nome;
+    private String username;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}

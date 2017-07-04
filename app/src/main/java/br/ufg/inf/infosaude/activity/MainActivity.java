47cc46bbe879dfba67e59202122d3243fe6bcd06
astr1x <lucaslupus1@gmail.com>
2017-07-03 22:50:57 -0300

    Renomeando arquivos fora do padrao

--- app/src/main/java/br/ufg/inf/infosaude/activity/MainActivity.java
+++ app/src/main/java/br/ufg/inf/infosaude/activity/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends BaseActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.act_principal, menu);
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
         return true;
     }
 

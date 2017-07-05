9ca00a591919ec021803bdae7564a82ed717ff84
mariomjr <mariohj94@gmail.com>
2017-07-04 23:14:15 -0300

    Auto stash before rebase of "origin/master"
    Busca dos hospitais próximos e marcação no mapa

--- app/src/main/java/br/ufg/inf/infosaude/fragment/MapFragment.java
+++ app/src/main/java/br/ufg/inf/infosaude/fragment/MapFragment.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -14,7 +16,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -29,8 +30,22 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 import br.ufg.inf.infosaude.R;
+import br.ufg.inf.infosaude.utils.GPSTracker;
 import br.ufg.inf.infosaude.utils.MapUtils;
 
 import static android.app.Activity.RESULT_CANCELED;
@@ -38,13 +53,27 @@ import static android.app.Activity.RESULT_OK;
 import static android.content.ContentValues.TAG;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback,
-        GoogleApiClient.OnConnectionFailedListener{
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
 
     private static GoogleApiClient mGoogleApiClient;
 
     private static final int PLACE_AUTOCOMPLETE_REQUEST_CODE =1;
+
+    private static final int PROXIMITY_RADIUS =5000;
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    private static final String GOOGLE_BROWSER_API_KEY= "AIzaSyA016QgQU5d_sClXD6nc7iQg8Jo3CF1_k0";
+
+    public static final String LATITUDE = "lat";
+    public static final String LONGITUDE = "lng";
+    public static final String GEOMETRY = "geometry";
+    public static final String ICON = "icon";
+    public static final String PLACE_NAME = "place_name";
+    public static final String NAME = "name";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,6 +83,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        checkLocationPermission();
 
         if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
@@ -74,13 +108,49 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
         mMap.getUiSettings().setMapToolbarEnabled(false);
         mMap.getUiSettings().setRotateGesturesEnabled(false);
         mMap.getUiSettings().setZoomControlsEnabled(false);
+        mMap.setOnMarkerClickListener(this);
 
         Location localizacaoAtual = MapUtils.retornaLocalizacaoAtual(getContext());
+        GPSTracker location = new GPSTracker(getContext());
 
- //       MapUtils.zoomToLocation(mMap, new LatLng(localizacaoAtual.getLatitude(), localizacaoAtual.getLongitude()), MapUtils.ZOOM_FACTOR);
-        MapUtils.zoomToLocation(mMap, new LatLng(-16.6773716, -49.2458615), MapUtils.ZOOM_FACTOR);
+        MapUtils.zoomToLocation(mMap, new LatLng(location.getLatitude(), location.getLongitude()), MapUtils.ZOOM_FACTOR);
+      //  MapUtils.zoomToLocation(mMap, new LatLng(-16.6773716, -49.2458615), MapUtils.ZOOM_FACTOR);
+
+        loadNearByPlaces(location.getLatitude(),location.getLongitude());
 
         initApiPlace();
+        mGoogleApiClient.connect();
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this.getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this.getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this.getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
@@ -89,7 +159,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
                 super.getActivity().getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
 
         AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
-                .setTypeFilter(Place.TYPE_COUNTRY).setCountry("BR")
                 .setTypeFilter(Place.TYPE_HOSPITAL)
                 .build();
         autocompleteFragment.setFilter(typeFilter);
@@ -97,14 +166,15 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
-                // TODO: Get info about the selected place.
-                Log.i(TAG, "Place: " + place.getName());//get place details here
+                LatLng latLong = place.getLatLng();
+                MapUtils.zoomToLocation(mMap, new LatLng(latLong.latitude, latLong.longitude), MapUtils.ZOOM_FACTOR);
+                mMap.addMarker(new MarkerOptions().position(latLong).title(place.getName().toString()));
             }
 
             @Override
             public void onError(Status status) {
 
-                Log.i(TAG, "An error occurred: " + status);
+                Log.i(TAG, "Erro: " + status);
             }
         });
     }
@@ -132,6 +202,81 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
                 .addApi(Places.PLACE_DETECTION_API)
                 .enableAutoManage(getActivity(), this)
                 .build();
+    }
+
+    private void loadNearByPlaces(double latitude, double longitude) {
+
+        ArrayList resultList = null;
+        HttpURLConnection conn = null;
+        StringBuilder jsonResults = new StringBuilder();
+        try {
+            String type = "hospital";
+            StringBuilder sb =
+                    new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+            sb.append("location=").append(latitude).append(",").append(longitude);
+            sb.append("&radius=").append(PROXIMITY_RADIUS);
+            sb.append("&types=").append(type);
+            sb.append("&sensor=true");
+            sb.append("&key=" + GOOGLE_BROWSER_API_KEY);
+
+            URL url = new URL(sb.toString());
+
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                jsonResults.append(buff, 0, read);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        double latitudeAux, longitudeAux;
+        String placeName = "";
+        try {
+            // Create a JSON object hierarchy from the results
+            JSONObject jsonObj = new JSONObject(jsonResults.toString());
+            JSONArray predsJsonArray = jsonObj.getJSONArray("results");
+
+            for (int i = 0; i < predsJsonArray.length(); i++) {
+                JSONObject place = predsJsonArray.getJSONObject(i);
+
+
+                if (!place.isNull(NAME)) {
+                    placeName = place.getString(NAME);
+                }
+
+                latitudeAux = place.getJSONObject(GEOMETRY).getJSONObject("location")
+                        .getDouble(LATITUDE);
+                longitudeAux = place.getJSONObject(GEOMETRY).getJSONObject("location")
+                        .getDouble(LONGITUDE);
+                //reference = place.getString(REFERENCE);
+                //icon = place.getString(ICON);
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                LatLng latLng = new LatLng(latitudeAux, longitudeAux);
+                markerOptions.position(latLng);
+                markerOptions.title(placeName);
+
+                mMap.addMarker(markerOptions);
+
+            }
+        } catch (JSONException e) {
+            // Log.e(LOG_TAG, "Cannot process JSON results", e);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     //FIXME japa, ver a necessidade de ter isso aqui dentro desse fragment
@@ -172,5 +317,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Log.d(TAG, "Lat.-"+marker.getPosition().latitude+" Long.-"+marker.getPosition().longitude);
+        return false;
     }
 }

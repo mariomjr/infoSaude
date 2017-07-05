52bd954c907b8e1068fee53cd3611507b3ed4970
mariomjr <mariohj94@gmail.com>
2017-07-05 19:30:20 -0300

    Auto stash before merge of "master" and "origin/master"
    Configuração de telas

--- app/src/main/java/br/ufg/inf/infosaude/services/HospitalService.java
+++ app/src/main/java/br/ufg/inf/infosaude/services/HospitalService.java
@@ -2,9 +2,11 @@ package br.ufg.inf.infosaude.services;
 
 import java.util.List;
 
+import br.ufg.inf.infosaude.model.Especialidades;
 import br.ufg.inf.infosaude.model.Hospital;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * Created by MarioJr on 28/06/2017.
@@ -15,4 +17,6 @@ public interface HospitalService {
     @GET("hospitais")
     Call<List<Hospital>> getListHospitais();
 
+    @GET("especialidades/")
+    Call<List<Especialidades>> getListEspecialidades(@Query("latitudade") Double latitude, @Query("longitude") Double longitude);
 }
